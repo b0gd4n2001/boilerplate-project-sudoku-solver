4c76ad4d1789054ab00ed47136d3981c71480a8c
b0gd4n2001 <lulelarub@gmail.com>
2024-07-01 14:11:07 +0300

    feat: game logic & tests

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,3 +1,5 @@
+let strings = require('./puzzle-strings.js').puzzlesAndSolutions
+
 class SudokuSolver {
   constructor() {
     this.rowToNum = {
@@ -7,9 +9,9 @@ class SudokuSolver {
       d: 27,
       e: 36,
       f: 45,
-      h: 54,
-      i: 63,
-      j: 72
+      g: 54,
+      h: 63,
+      i: 72
     }
     this.rowToSpot = {
       a: 1,
@@ -43,6 +45,17 @@ class SudokuSolver {
       7: 54,
       8: 57,
       9: 60
+    }
+    this.regToColRow = {
+      1: 'abc123',
+      2: 'abc456',
+      3: 'abc789',
+      4: 'def123',
+      5: 'def456',
+      6: 'def789',
+      7: 'ghi123',
+      8: 'ghi456',
+      9: 'ghi789'
     }
   }
 
@@ -78,29 +91,74 @@ class SudokuSolver {
     if (!(puzzleString.match(/^[1-9\.]+$/)[0].length == 81)) return false;
     let regex = /([1-9]).*\1/
     for (let i = 0; i < 9; i++) {
-      if(regex.test(this.row(puzzleString, this.spotToRow[i]))) return false;
-      if(regex.test(this.col(puzzleString, i))) return false;
-      if(regex.test(this.reg(puzzleString, i))) return false;
+      if (regex.test(this.row(puzzleString, this.spotToRow[i]))) return false;
+      if (regex.test(this.col(puzzleString, i))) return false;
+      if (regex.test(this.reg(puzzleString, i))) return false;
     }
     return true
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-    
+    value = `${value}`;
+    let str = this.row(puzzleString, row).split('');
+    str[column - 1] = '.';
+    return !(str.includes(value));
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    value = `${value}`;
+    let str = this.col(puzzleString, column).split('');
+    str[this.rowToSpot[row] - 1] = '.';
+    return !(str.includes(value));
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    value = `${value}`
+    let puzzleArray = puzzleString.split('');
+    let region;
+    puzzleArray[this.rowToNum[row] + column - 1] = '.';
+    for (let i = 1; i <= 9; i++) {
+      if (!(this.regToColRow[i].includes(row))) continue;
+      if (!(this.regToColRow[i].includes(`${column}`))) continue;
+      region = i;
+      break;
+    };
+    return !(this.reg(puzzleArray.join(''), region).includes(value));
   }
 
   solve(puzzleString) {
+    if (!puzzleString.includes('.')) return puzzleString
+    let puzzleArray = puzzleString.split('');
+    let posible;
+    let lastIndex;
+    for (let i = 0; i < 81; i++) {
+      if (puzzleString[i] != '.') continue;
+      posible = [];
+      let row = this.spotToRow[Math.ceil((i + 1) / 9)];
+      let column = (i % 9) + 1;
+      for (let value = 1; value <= 9; value++) {
+        if (!(this.checkRowPlacement(puzzleString, row, column, value))) continue;
+        if (!(this.checkColPlacement(puzzleString, row, column, value))) continue;
+        if (!(this.checkRegionPlacement(puzzleString, row, column, value))) continue;
+        posible.push(value);
+      }
+      if (posible.length == 0) return null;
+      lastIndex = i;
+      if (posible.length == 1) {
+        puzzleArray[i] = posible[0];
+        return this.solve(puzzleArray.join(''));
+      }
+    }
+    for (let j of posible) {
+      puzzleArray[lastIndex] = j;
+      let maybe = this.solve(puzzleArray.join(''));
+      if (maybe) return maybe;
+    };
 
   }
 }
+
+let solver = new SudokuSolver;
 
 module.exports = SudokuSolver;
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,6 +5,8 @@ const Solver = require('../controllers/sudoku-solver.js');
 let solver = new Solver;
 let strings = require('../controllers/puzzle-strings.js').puzzlesAndSolutions;
 
+
+
 suite('Unit Tests', () => {
     test('validate', function () {
         assert.equal(solver.validate(strings[0][0]), true);
@@ -15,7 +17,25 @@ suite('Unit Tests', () => {
     test('col', function () {
         assert.equal(solver.col(strings[0][0], '1'), '1...834.2');
     });
-    test('reg', function() {
+    test('reg', function () {
         assert.equal(solver.reg(strings[0][0], '1'), '1.5..6.2.');
     });
+    test('checkRowPlacement', function () {
+        assert.equal(solver.checkRowPlacement(strings[0][0], 'a', 2, 3), true);
+    });
+    test('checkColPlacement', function () {
+        assert.equal(solver.checkColPlacement(strings[0][0], 'a', 2, 3), true);
+    });
+    test('checkRegionPlacement', function () {
+        assert.equal(solver.checkRegionPlacement(strings[0][0], 'a', 2, 3), true);
+    });
+    test('solve', function () {
+        assert.equal(solver.solve(strings[0][0]), strings[0][1]);
+        assert.equal(solver.solve(strings[1][0]), strings[1][1]);
+        assert.equal(solver.solve(strings[2][0]), strings[2][1]);
+        assert.equal(solver.solve(strings[3][0]), strings[3][1]);
+        assert.equal(solver.solve(strings[4][0]), strings[4][1]);
+    });
+
 });
+
